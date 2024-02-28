48a45e8338122cd902f20e284ec0912302a873a4
ddkogit <git config --global user.email>
2024-02-28 17:25:31 +0545

    navbar design done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <>
-   Hello
+   <Home />
    </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar">
+        <div>
+          <button className="logo">D</button>
+        </div>
+
+        <div>
+          <span>Projects</span>
+
+          <button className="hireme-button">Hier me</button>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
