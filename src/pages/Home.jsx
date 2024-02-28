a72974da0a8b508148666b9272d941810515134b
ddkogit <git config --global user.email>
2024-02-28 17:48:50 +0545

    home page design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,20 @@ function Home() {
   return (
     <div className="home-container">
       <Navbar />
+
+      <div className="home-main">
+
+
+      <div className="h1 name">Dipesh Dulal</div>
+      <div className="h2">Frontend Developer</div>
+      <p className="home-content">
+        I develop websites using HTML, CSS and Javascript, specializing in
+        creating dunamic interfaces with React.js.
+      </p>
+
+      <img className="avatar-img" src="/avatarbg.png" alt="avatar image" />
+      <img className="landscape-img" src="/landbg.png" alt="avatar image" />
+      </div>
     </div>
   );
 }
