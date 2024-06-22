bd5506f28f0d87f507de86b9a879702a7655873a
ddkogit <git config --global user.email>
2024-06-22 17:53:25 +0545

    a project added

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,15 @@ const projectList = [
     webLink: "https://saiyansclub.vercel.app/",
   },
   {
+    title: "Pic Polish",
+    imgUrl:
+      "picpolish.png",
+    webLink: "https://pic-polish.vercel.app/",
+  },
+  {
     title: "Sketch Board",
     imgUrl: "sketch.png",
     webLink: "https://ddkogit.github.io/etch-a-sketch/",
-  },
-  {
-    title: "Comming Soon",
-    imgUrl:
-      "https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8d2Vic2l0ZXx8fHx8fDE3MDkxMzE0MTQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
-    webLink: "#",
   },
   {
     title: "Comming Soon",
